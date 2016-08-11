4bbef801202b5b7122c8d8ac751b1d8013909c12
Antonis Christofides <anthony@itia.ntua.gr>
2016-08-11 16:17:58 +0300

    Pass correct "headers" argument to requests.post
    The "headers" argument to requests.post is required to be a dictionary
    where the names and values are strings; however, Content-Length was an
    integer.

--- htmlvalidator/tests/test_client.py
+++ htmlvalidator/tests/test_client.py
@@ -3,6 +3,7 @@
 import codecs
 import os
 import shutil
+import sys
 import tempfile
 from glob import glob
 
@@ -60,3 +61,16 @@ class ClientTestCase(TestCase):
             txt_file, = glob(os.path.join(self.tmpdir, '*.txt'))
             with codecs.open(txt_file, encoding='utf-8') as f:
                 self.assertTrue(content in f.read())
+
+        # Make sure the "headers" argument to htmlvalidator.core.requests.post
+        # was a mapping of str to str.
+        using_python_2 = sys.version_info[0] < 3
+        bytestype = str if using_python_2 else bytes
+        stringtype = unicode if using_python_2 else str
+        post.assert_called()
+        headers = post.call_args[1]['headers']
+        for header in headers:
+            self.assertTrue(isinstance(header, stringtype) or
+                            isinstance(header, bytestype))
+            self.assertTrue(isinstance(headers[header], stringtype) or
+                            isinstance(headers[header], bytestype))
