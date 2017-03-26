a41491d0ce97207fd366c993f0f348fee1b6855a
Jon Dufresne <jon.dufresne@gmail.com>
2017-03-26 08:56:39 -0700

    Remove workarounds for unsupported Python and Django versions
    Fix trove classifiers for all supported versions.

--- htmlvalidator/tests/urls.py
+++ htmlvalidator/tests/urls.py
@@ -6,8 +6,3 @@ from htmlvalidator.tests import views
 urlpatterns = [
     url(r'^view/', views.html_view, name='html_view')
 ]
-try:
-    from django.conf.urls import patterns
-    urlpatterns = patterns('', *urlpatterns)
-except ImportError:  # Django >= 1.10
-    pass
